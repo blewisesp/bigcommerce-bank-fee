647bba7cd30187688ce8cc062a79dd0a8f482b19
blewisesp <lewis.robert@gmail.com>
2026-03-19 17:07:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.post('/add-bank-fee', async (req, res) => {
           name: 'bank_deposit_fee',
           display_name: 'Bank Transfer Fee',
           cost: 50.00,
+          type: 'surcharge',
           source: 'storefrontapi',
         }]
       }),
